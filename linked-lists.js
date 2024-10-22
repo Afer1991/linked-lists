8c41f384eeadd133fad4a88846ffa7b0afb190d1
Af1991 <152735889+Afer1991@users.noreply.github.com>
2024-10-21 21:57:02 -0400

    Create Node and LinkedList classes

---
+++ linked-lists.js
@@ -0,0 +1,53 @@
+class Node {
+  constructor(value = null) {
+    this.value = value;
+    this.nextNode = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+      
+  }
+
+  append(value) {
+      
+  }
+
+  prepend(value) {
+
+  }
+
+  size() {
+
+  }
+
+  head() {
+
+  }
+
+  tail() {
+
+  }
+
+  at(index) {
+
+  }
+
+  pop() {
+
+  }
+
+  contains(value) {
+
+  }
+
+  find(value) {
+
+  }
+
+  toString() {
+
+  }
+
+  }
